8590ebd2d0cd65bb022bd305bfcc07fd482f7f7a
AhmedMaghraoui <94858065+AhmedMaghraoui@users.noreply.github.com>
2023-05-21 21:18:46 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { LAYOUT, MENU_BEHAVIOUR, NAV_COLOR, MENU_PLACEMENT, RADIUS, THEME_COLOR,
 export const IS_DEMO = false;
 export const IS_AUTH_GUARD_ACTIVE = true;
 // export const SERVICE_URL = 'https://where-iz-api-v3.onrender.com/api/v1';
-export const SERVICE_URL = 'http://35.180.196.133:5000/api/v1';
+export const SERVICE_URL = 'https://api.wheriz.pro/api/v1';
 
 export const USE_MULTI_LANGUAGE = false;
 export const AUTH_TOKEN = 'token';
